314d649765d8c4430317ff4d003a371332ae2645
middleverse <arshi_b@hotmail.com>
2020-12-04 11:06:20 -0500

    basic view, returns minimal HTML

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,6 @@
 from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
 from lists.views import home_page
 # from lists.views import about_page
 
@@ -8,3 +9,10 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
    
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request) # returns HttpResponse object
+        html = response.content.decode('utf8') # response content is in binary and need to be decoded 
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>TODO Lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))

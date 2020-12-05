1e00630365e9bf7c9e60f29f5d133623e5d4cd44
middleverse <arshi_b@hotmail.com>
2020-12-05 10:28:34 -0500

    refactor home page view to use a template

--- lists/tests.py
+++ lists/tests.py
@@ -1,18 +1,15 @@
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
+
 from lists.views import home_page
-# from lists.views import about_page
+
 
 class HomePageTest(TestCase):
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-   
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request) # returns HttpResponse object
-        html = response.content.decode('utf8') # response content is in binary and need to be decoded 
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>TODO Lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+    def test_uses_home_template(self):
+        response = self.client.get('/')  # Django Test Client tool
+        self.assertTemplateUsed(response, 'home.html')
+
+        
+    
--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse
 
 def home_page(request):
-    return HttpResponse('<html><title>TODO Lists</title></html>')
+    return render(request, 'home.html') # takes request and name of template to render
+    
